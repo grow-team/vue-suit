4c6f6300e3b0a21bde363d8babbb45a29db5b119
jialj <514435903@qq.com>
2018-08-28 00:06:29 +0800

    添加github链接

--- src/components/frame/headbar/index.vue
+++ src/components/frame/headbar/index.vue
@@ -14,6 +14,9 @@
             小明<i class="el-icon-arrow-down el-icon--right"></i>
           </span>
           <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>
+              <a target="_blank" href="https://github.com/grow-team/vue-suit">github</a>
+            </el-dropdown-item>
             <el-dropdown-item>个人中心</el-dropdown-item>
             <el-dropdown-item>设置</el-dropdown-item>
             <el-dropdown-item>退出</el-dropdown-item>
--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/app/',
   routes: [
     ...sideMenu
   ]
